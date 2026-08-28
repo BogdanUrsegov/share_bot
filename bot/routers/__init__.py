6c803689a37d90b5b5f8341af29a40189bb4ec68
BogdanUrsegov <102915294+BogdanUrsegov@users.noreply.github.com>
2026-08-28 17:43:44 +0500

    feat: register referral router

--- bot/routers/__init__.py
+++ bot/routers/__init__.py
@@ -4,15 +4,16 @@ from bot.modules.admin import router as admin_router
 from bot.modules.show import router as show_router
 from bot.modules.profile import router as profile_router
 from bot.modules.earn import router as earn_router
+from bot.modules.referral import router as referral_router
 
 router = Router()
 router.include_routers(
-                        start_router,
-                        admin_router,
-                        show_router,
-                        profile_router,
-                        earn_router
-                    )
-
+    start_router,
+    admin_router,
+    show_router,
+    profile_router,
+    earn_router,
+    referral_router,
+)
 
 __all__ = ["router"]
